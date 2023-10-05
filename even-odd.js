134a551d4bf632bac21bba2a2277c8de83be5db9
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-06 00:00:08 +0600

    ok

---
+++ even-odd.js
@@ -0,0 +1,13 @@
+let a=1;
+while(a<7){
+    if(a%2==0)
+        console.log(a);
+    
+    else
+    console.log(a);
+a +=1;
+}
+
+function newFunction() {
+    return "is even";
+}
---
+++ slice.js
@@ -0,0 +1,2 @@
+var numbers=[1,4,7,98,33,44,32,65];
+numbers.slice(2,4);
